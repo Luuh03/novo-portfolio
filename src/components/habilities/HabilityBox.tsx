e1bc019ed448185a2a452ef1f7ab25857e62124a
Luan Queiroz <luan.queiroz2003@gmail.com>
2025-11-06 18:57:17 -0300

    :lipstick: Ajustes navbar e ícone de habilidades

--- src/components/habilities/HabilityBox.tsx
+++ src/components/habilities/HabilityBox.tsx
@@ -11,8 +11,8 @@ interface HabilityProps {
 function HabilityBox({ hability }: HabilityProps) {
   return (
     <div className="flex flex-col items-center py-3 w-[7rem] bg-(--card-background) rounded-lg gap-2
-      transition-all duration-250 hover:scale-115">
-      <img className="w-16" src={hability.image} alt={hability.name} />
+      transition-all duration-250 hover:scale-115 shadow-lg/40">
+      <img className="h-16" src={hability.image} alt={hability.name} />
       <span className="text-white text-sm">{hability.name}</span>
     </div>
   )
